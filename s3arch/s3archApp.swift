39fb3d12b76eb49f9df42ee107de440c55129e95
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-11-02 13:35:16 -0700

    Initial Commit

---
+++ s3arch/s3archApp.swift
@@ -0,0 +1,17 @@
+//
+//  s3archApp.swift
+//  s3arch
+//
+//  Created by Nick Reisenauer on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct s3archApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
